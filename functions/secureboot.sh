0119bf38cd199187a3959a3692791b237fef2109
Ewan Clark <ewanclark4312@gmail.com>
2025-11-21 22:29:56 +0000

    fix: update log message for Secure Boot enrollment to clarify that it enables Secure Boot

--- functions/secureboot.sh
+++ functions/secureboot.sh
@@ -14,7 +14,7 @@ secure_boot_info() {
   log_success "Secure Boot has been enabled."
   log_info "Restart your computer, enter BIOS, and enable/restart into Secure Boot setup mode."
   log_info "Boot into the new Arch installation and run the setup script found in your home directory."
-  log_info "Once Secure Boot keys are enrolled, you'll be able to boot with Secure Boot enforced."
+  log_info "Once Secure Boot keys are enrolled, you'll be able to boot with Secure Boot enabled."
   log_info "Delete the setup script from your home directory after completing the steps."
   log_blank
   log_warn "IMPORTANT: Reboot into Secure Boot setup mode before running the setup script or it will fail."
