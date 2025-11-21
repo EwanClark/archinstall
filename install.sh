b4319ded00c301c74fe32c5299f4e2f2929b6280
Ewan Clark <ewanclark4312@gmail.com>
2025-11-21 19:21:45 +0000

    fix: replace unzip with bsdtar to work on arch iso

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 set -Eeuo pipefail
 
 curl -L https://github.com/EwanClark/archinstall/archive/refs/heads/main.zip -o archinstall.zip
-unzip -q archinstall.zip
+bsdtar -xf archinstall.zip
 cd archinstall-main
 chmod +x init.sh
 ./init.sh
