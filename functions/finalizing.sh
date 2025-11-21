0b4b3c8f7669b49b140939510f74434830a77352
Ewan Clark <ewanclark4312@gmail.com>
2025-11-21 20:16:21 +0000

    fix: update secure boot condition check to handle unset variable

--- functions/finalizing.sh
+++ functions/finalizing.sh
@@ -1,7 +1,7 @@
 finalizing() {
   mkinitcpio -P
   log_step "Installation complete!"
-  if secure_boot; then
+  if [[ "${secure_boot:-false}" == "true" ]]; then
     secure_boot_info
     generate_script
     log_info "You can now reboot your system. Remember to enable Secure Boot setup mode!"
