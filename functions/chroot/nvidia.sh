38a95ec373bf1f2f917712bd5cbb14f024970a85
Ewan Clark <ewanclark4312@gmail.com>
2025-11-22 20:33:46 +0000

    fix: remove space after ( in mkinitcpio.conf for nvidia

--- functions/chroot/nvidia.sh
+++ functions/chroot/nvidia.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 nvidia() {
   sed -i '/^GRUB_CMDLINE_LINUX_DEFAULT=/s/".*"/"loglevel=3 nvidia-drm.modeset=1"/' /etc/default/grub
-  sed -i '/^MODULES=/s/)/ nvidia nvidia_modeset nvidia_uvm nvidia_drm)/' /etc/mkinitcpio.conf
+  sed -i '/^MODULES=/ {/nvidia/!{s/()/(nvidia nvidia_modeset nvidia_uvm nvidia_drm)/;t;s/)/ nvidia nvidia_modeset nvidia_uvm nvidia_drm)/;}}' /etc/mkinitcpio.conf
   # mkinitcpio -P     running it at the end of arch install process.
 }
